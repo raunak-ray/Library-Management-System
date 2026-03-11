33e5880cd27d8132c5795a6284b5350cb275ef3d
Raunak Ray <raunakay3260@gmail.com>
2026-03-11 21:14:58 +0530

    add login and signup functionality with form handling, validation, and API integration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
 app.use(cookieParser())
 
 app.use(express.json())
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,34 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster
+  position="top-right"
+  gutter={10}
+  toastOptions={{
+    duration: 3500,
+    style: {
+      background: "oklch(0.145 0 0)",
+      color: "#ffffff",
+      border: "1px solid #e5e5e5",
+      padding: "14px 16px",
+      borderRadius: "8px",
+      fontSize: "18px",
+      boxShadow: "0 4px 12px rgba(0,0,0,0.08)",
+    },
+    success: {
+      iconTheme: {
+        primary: "#000",
+        secondary: "#fff",
+      },
+    },
+    error: {
+      iconTheme: {
+        primary: "#000",
+        secondary: "#fff",
+      },
+    },
+  }}
+/>
       </body>
     </html>
   );
