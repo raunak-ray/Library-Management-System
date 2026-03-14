5137569357f1664fef6a32bcfcdcdda7ac48b20a
Raunak Ray <raunakay3260@gmail.com>
2026-03-14 17:05:27 +0530

    Remove conditional BASE_URL setup and simplify to a single production URL

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// const BASE_URL = process.env.NODE_ENV === "production" ? 
+//                     "https://library-management-system-8k77.onrender.com/api/v1" :
+//                     "http://localhost:5500/api/v1";
+
+const BASE_URL = "https://library-management-system-8k77.onrender.com/api/v1"
+
+export const api = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
