33ec6e22b7c8cc64f105268a16084d7b6abf04ed
Raunak Ray <raunakay3260@gmail.com>
2026-03-14 17:14:34 +0530

    Update cookie settings for token storage: set secure to true and sameSite to none

--- backend/src/utils/jwtToken.ts
+++ backend/src/utils/jwtToken.ts
@@ -19,8 +19,8 @@ export const verifyToken = (token: string) => {
 export const storeTokenInCookie = (res: any, token: string) => {
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        secure: true,
+        sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000
     });
-}
+};
